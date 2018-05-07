062f78a2d60f529b56dd6a8c7794bd0ed043d700
rujingzhuo <rujingz@126.com>
2018-05-07 10:18:32 +0800

    初始化

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './index.scss';
+
+ReactDOM.render(
+  <div>
+    here
+    <h1>Hello, world!</h1>
+  </div>,
+  document.getElementById('app')
+);
