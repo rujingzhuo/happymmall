d9867e65d071812929e6ad9e21abb12b1f942e94
rujingzhuo <rujingz@126.com>
2018-05-08 17:20:30 +0800

    用户列表及首页错误页开发

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
-// import 'font-awesome/css/font-awesome.min.css';
-// import './index.css';
-// import './index.scss';
+
 import Layout from 'component/layout/index.jsx';
 
 import Home from 'page/home/index.jsx';
+import Login from 'page/login/index.jsx';
+import UserList from 'page/user/index.jsx';
+import ErrorPage from 'page/error/index.jsx';
 
 class App extends React.Component{
   render(){
       return (
         <Router>
-          <Layout>
-            <Switch>
-              <Route exact path="/" component={Home}/>
-              <Redirect path="*" to="/"/>
-            </Switch>
-          </Layout>
+          <Switch>
+            <Route path="/login" component={Login}/>
+            <Route path="/" render={props => (
+              <Layout>
+                <Switch>
+                  <Route exact path="/" component={Home}/>
+                  <Route exact path="/user/index" component={UserList}/>
+                  <Redirect exact from="/user" to="/user/index"/>
+                  <Route component={ErrorPage}/>
+                </Switch>
+              </Layout>
+            )}/>
+          </Switch>
         </Router>
       )
   }
